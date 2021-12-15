e320798f91a6b70f02a41a2d648a35066a465e06
Jay251287 <artham.swathi@gmail.com>
2021-12-15 12:28:50 +0530

    Created branch added classes

---
+++ src/test/java/com/practise/Gitbash4.java
@@ -0,0 +1,10 @@
+package com.practise;
+
+public class Gitbash4 {
+
+	public static void main(String[] args) {
+		System.out.println("Trying to create branch");
+
+	}
+
+}
---
+++ src/test/java/com/practise/Gitbash5.java
@@ -0,0 +1,11 @@
+package com.practise;
+
+public class Gitbash5 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Merging to be done");
+	}
+
+}

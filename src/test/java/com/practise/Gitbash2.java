42c1d728cbb42187ed04fb3e7d792e7e6344e1f5
Jay251287 <artham.swathi@gmail.com>
2021-12-15 12:14:55 +0530

    new classes added now

---
+++ src/test/java/com/practise/Gitbash2.java
@@ -0,0 +1,13 @@
+package com.practise;
+
+public class Gitbash2 {
+
+	public static void main(String[] args) {
+		int a = 10, b=20;
+		
+		int result = a+b;
+		
+		System.out.println(result);
+		
+	}
+}
